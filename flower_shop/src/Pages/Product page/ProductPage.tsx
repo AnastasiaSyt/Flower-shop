da9a4a20574b8dc0de86c9a2d199f763c3d19005
Anastasia Sytsevich <drozdushka@gmail.com>
2023-09-13 20:51:45 +0300

    feat: add pop-up message

--- flower_shop/src/Pages/Product page/ProductPage.tsx
+++ flower_shop/src/Pages/Product page/ProductPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import image from '../../assets/flowers-green-leaves.jpg';
 import './ProductPage.scss';
 import { useLocation } from 'react-router-dom';
@@ -7,11 +7,13 @@ import Button from '../../components/Button/Button';
 import Breadcrumbs from '../../components/Breadcrumbs/Breadcrumbs';
 import FlowerTag from '../../components/FlowerTag/FlowerTag';
 import { addItem } from '../../Redux/cartSlice';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../Redux/store';
 
 function ProductPage() {
   const location = useLocation();
   const dispatch = useDispatch();
+  const cartItems = useSelector((state: RootState) => state.cart.cartItems);
   const cardData = location.state && location.state.cardData;
 
   const [selectedImage, setSelectedImage] = useState(
@@ -20,6 +22,18 @@ function ProductPage() {
 
   const [isHovered, setIsHovered] = useState(false);
   const [quantity, setQuantity] = useState(1);
+  const [isMessageVisible, setMessageVisible] = useState(false);
+  const [buttonText, setButtonText] = useState('Добавить в корзину');
+
+  useEffect(() => {
+    const isItemInCart = cartItems.some((item) => item.productId === cardData?.id);
+
+    if (isItemInCart) {
+      setButtonText('Добавить еще');
+    } else {
+      setButtonText('Добавить в корзину');
+    }
+  }, [cartItems, cardData]);
 
   const handleMouseEnter = () => {
     setIsHovered(true);
@@ -47,6 +61,18 @@ function ProductPage() {
           image: thumbnail,
         })
       );
+      setButtonText('Товар добавлен');
+      setMessageVisible(true);
+
+      setTimeout(() => {
+        setMessageVisible(false);
+        const isItemInCart = cartItems.some((item) => item.productId === cardData.id);
+        if (isItemInCart) {
+          setButtonText('Добавить еще');
+        } else {
+          setButtonText('Добавить в корзину');
+        }
+      }, 2000);
     }
   };
 
@@ -91,7 +117,7 @@ function ProductPage() {
               ))}
             </div>
             <p className="card_product_description">{cardData.description}</p>
-            <p className="card_product_sort">В наличии: {cardData.stock} шт</p>
+            <p className="card_product_sort">В наличии: {cardData.stock - quantity} шт</p>
             <div className="card_product_color">
               Цвет:
               {cardData.color.map((item: string, index: number) => (
@@ -104,8 +130,13 @@ function ProductPage() {
             </div>
             <p className="card_product_price">${cardData.price}</p>
             <div className="card_product_buttons">
-              <Button className="card_product_buttons_cart" onClick={handleAddToCart}>
-                добавить в корзину
+              {isMessageVisible && <div className="popup-message">Товар добавлен в корзину</div>}
+              <Button
+                className="card_product_buttons_cart"
+                onClick={handleAddToCart}
+                disabled={isMessageVisible}
+              >
+                {buttonText}
               </Button>
               <ProductsCounter
                 quantity={quantity}
